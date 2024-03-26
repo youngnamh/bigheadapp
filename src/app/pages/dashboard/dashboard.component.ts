6c7c602b316b8dade44a02a0e04f3000eda870d6
youngnamh <youngnamhlady@gmail.com>
2024-03-26 18:29:28 -0400

    redirect.html

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DashContentComponent } from '../../components/dash-content/dash-content.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,4 +9,16 @@ import { DashContentComponent } from '../../components/dash-content/dash-content
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      const originalUrl = params['originalUrl'];
+      if (originalUrl) {
+        console.log('Original URL from dashboard:', originalUrl);
+        // Now you can use the originalUrl as needed in your Angular component logic
+      }
+    });
+  }
+}
