913f8cd7794f9630dc652522ccd860107cc2e22f
youngnamh <youngnamhlady@gmail.com>
2024-03-26 18:19:02 -0400

    redirect

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Injectable({
@@ -8,6 +8,21 @@ export class AuthService {
   constructor(private route: ActivatedRoute) {}
 
   getCodeFromUrl(): string | null {
+    const fullUrl = window.location.href;
+    console.log(`fullUrl: ${fullUrl}`);
+    const code = this.route.snapshot.queryParamMap.get('code');
+    console.log(`code: ${code}`);
     return this.route.snapshot.queryParamMap.get('code');
+  }
+
+  ngOnInit(): void {
+    this.route.queryParamMap.subscribe((params) => {
+      // Get the code parameter from the query params
+      console.log('subscribe Params:', params);
+      const code = params.get('code');
+      if (code) {
+        console.log('subscribe Code:', code);
+      }
+    });
   }
 }
