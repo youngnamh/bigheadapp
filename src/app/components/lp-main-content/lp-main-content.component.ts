decda3a2918ad35320375be0490d44ed27f93b65
youngnamh <youngnamhlady@gmail.com>
2024-03-26 21:59:18 -0400

    changing url

--- src/app/components/lp-main-content/lp-main-content.component.ts
+++ src/app/components/lp-main-content/lp-main-content.component.ts
@@ -11,10 +11,9 @@ import { FooterComponent } from '../footer/footer.component';
   styleUrl: './lp-main-content.component.scss',
 })
 export class LpMainContentComponent {
-  stravaAuth: string =
-    'http://www.strava.com/oauth/authorize?client_id=123612&response_type=code&redirect_uri=http://localhost/exchange_token&approval_prompt=force&scope=read';
   clientId: string = '123612';
-  clientSecret: string = 'a5530b9a4d2dbbd1a80a13a3789c36cda48f8cb9';
+  stravaAuth: string = `http://www.strava.com/oauth/authorize?client_id=${this.clientId}&response_type=code&redirect_uri=http://localhost/exchange_token&approval_prompt=force&scope=read`;
+  clientSecret: string = 'cf0a978e472f1ac06eb9a8782e19766933276a9f';
   code: string = '';
   authUrl: string = `https://www.strava.com/api/v3/oauth/token?client_id=${this.clientId}&client_secret=${this.clientSecret}&code=${this.code}&grant_type=authorization_code`;
 
