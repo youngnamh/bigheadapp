a77e674b391cb7951ea58ac8c12d4c2956f7dda6
youngnamh <youngnamhlady@gmail.com>
2024-03-31 16:02:34 -0400

    added everything to redirect successfully

--- src/app/components/redirect/redirect.component.ts
+++ src/app/components/redirect/redirect.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { StateService } from '../../services/state.service';
+import { Athlete } from '../../interfaces/Athlete';
 
 @Component({
   selector: 'app-redirect',
@@ -9,7 +12,14 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrl: './redirect.component.css',
 })
 export class RedirectComponent {
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  private athleteInfo!: Athlete;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private authService: AuthService,
+    private stateService: StateService
+  ) {}
 
   ngOnInit(): void {
     // get the full url from the browser
@@ -17,13 +27,36 @@ export class RedirectComponent {
     const code = this.route.snapshot.queryParamMap.get('code');
     if (code) {
       //console.log(`Setting redirect code in localStorage: ${code}`);
-      localStorage.setItem('redirect', code);
-
-      // Navigate to the desired route
-      this.router.navigateByUrl('/dashboard');
+      if (code) {
+        this.sendTokenToApi(code);
+      } else {
+        console.error('No code found in dashboard');
+      }
     } else {
       // Handle if no redirect URL is found
       console.error('No redirect URL found');
     }
+  }
+
+  sendTokenToApi(token: string): void {
+    this.authService.postStravaAuth(token).subscribe(
+      (response) => {
+        //console.log('API Response:', response);
+        this.createAthleteInfo(response);
+        this.router.navigateByUrl('/dashboard');
+      },
+      (error) => {
+        console.error('API Error:', error);
+        // Handle error
+      }
+    );
+  }
+
+  createAthleteInfo(response: string): void {
+    this.athleteInfo = JSON.parse(response);
+
+    this.stateService.setMainUser(this.athleteInfo);
+
+    console.log('Athlete Info:', this.athleteInfo);
   }
 }
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -17,47 +17,38 @@ export class DashboardComponent {
 
   constructor(
     private route: ActivatedRoute,
-    private authService: AuthService
+    private stateService: StateService
   ) {}
 
-  ngOnInit(): void {
-    const code = localStorage.getItem('redirect');
-    console.log(`Redirect code in Dash: ${code}`);
+  // ngOnInit(): void {
+  //   const code = localStorage.getItem('redirect');
+  //   console.log(`Redirect code in Dash: ${code}`);
 
-    // if (code) {
-    //   this.authService.postStravaAuth(code).subscribe(
-    //     (data) => {
-    //       console.log('Strava data: ', data);
-    //     },
-    //     (error) => {
-    //       console.error('Error fetching Strava data');
-    //     }
-    //   );
-    // }
+  //   if (code) {
+  //     this.sendTokenToApi(code);
+  //   } else {
+  //     console.error('No code found in dashboard');
+  //   }
+  // }
 
-    if (code) {
-      this.sendTokenToApi(code);
-    } else {
-      console.error('No code found in dashboard');
-    }
-  }
+  // sendTokenToApi(token: string): void {
+  //   this.authService.postStravaAuth(token).subscribe(
+  //     (response) => {
+  //       //console.log('API Response:', response);
+  //       this.createAthleteInfo(response);
+  //     },
+  //     (error) => {
+  //       console.error('API Error:', error);
+  //       // Handle error
+  //     }
+  //   );
+  // }
 
-  sendTokenToApi(token: string): void {
-    this.authService.postStravaAuth(token).subscribe(
-      (response) => {
-        //console.log('API Response:', response);
-        this.createAthleteInfo(response);
-      },
-      (error) => {
-        console.error('API Error:', error);
-        // Handle error
-      }
-    );
-  }
+  // createAthleteInfo(response: string): void {
+  //   this.athleteInfo = JSON.parse(response);
 
-  createAthleteInfo(response: string): void {
-    this.athleteInfo = JSON.parse(response);
+  //   this.stateService.setMainUser(this.athleteInfo);
 
-    console.log('Athlete Info:', this.athleteInfo);
-  }
+  //   console.log('Athlete Info:', this.athleteInfo);
+  // }
 }
