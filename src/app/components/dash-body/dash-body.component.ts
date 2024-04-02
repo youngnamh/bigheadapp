0a220e1813de02af1ac4b5e366244bd381ed8545
youngnamh <youngnamhlady@gmail.com>
2024-04-01 23:12:00 -0400

    if main user is added it is shown

--- src/app/components/dash-body/dash-body.component.ts
+++ src/app/components/dash-body/dash-body.component.ts
@@ -19,6 +19,16 @@ import { OnInit } from '@angular/core';
 export class DashBodyComponent {
   constructor(private stateService: StateService) {}
   participants = [{ name: 'Feven' }, { name: 'Koichi' }, { name: 'Rory' }];
+  mainUser: Athlete | null = null;
+  mainUserInitiated = false;
+
+  ngOnInit(): void {
+    this.mainUser = this.stateService.getMainUser();
+    if (this.mainUser !== null) {
+      this.mainUserInitiated = true;
+    }
+    console.log('mainUserInitiated: ', this.mainUserInitiated);
+  }
 
   ATHLETES: Athlete[] = [
     {
@@ -33,7 +43,11 @@ export class DashBodyComponent {
         { start_date: '2024-03-03T12:04:53Z', type: 'Run', distance: 1027 },
         { start_date: '2024-03-07T12:04:53Z', type: 'Run', distance: 2456 },
         { start_date: '2024-03-08T12:04:53Z', type: 'Run', distance: 8112 },
-        { start_date: '2024-03-10T12:04:53Z', type: 'Cycling', distance: 1507 },
+        {
+          start_date: '2024-03-10T12:04:53Z',
+          type: 'Cycling',
+          distance: 1507,
+        },
       ],
       Groups: ['A1001'],
       Friends: [64603272],
@@ -47,16 +61,28 @@ export class DashBodyComponent {
       listLength: 7,
       Activities: [
         { start_date: '2024-03-01T12:04:53Z', type: 'Run', distance: 7002 },
-        { start_date: '2024-03-05T12:04:53Z', type: 'Cycling', distance: 2945 },
+        {
+          start_date: '2024-03-05T12:04:53Z',
+          type: 'Cycling',
+          distance: 2945,
+        },
         {
           start_date: '2024-03-06T12:04:53Z',
           type: 'Swimming',
           distance: 3472,
         },
         { start_date: '2024-03-09T12:04:53Z', type: 'Run', distance: 9681 },
-        { start_date: '2024-03-10T12:04:53Z', type: 'Cycling', distance: 1807 },
+        {
+          start_date: '2024-03-10T12:04:53Z',
+          type: 'Cycling',
+          distance: 1807,
+        },
         { start_date: '2024-03-12T12:04:53Z', type: 'Run', distance: 6210 },
-        { start_date: '2024-03-19T12:04:53Z', type: 'Cycling', distance: 1184 },
+        {
+          start_date: '2024-03-19T12:04:53Z',
+          type: 'Cycling',
+          distance: 1184,
+        },
       ],
       Groups: ['A1001'],
       Friends: [64603272],
@@ -70,16 +96,28 @@ export class DashBodyComponent {
       listLength: 8,
       Activities: [
         { start_date: '2024-03-03T08:30:45Z', type: 'Run', distance: 6325 },
-        { start_date: '2024-03-05T12:55:20Z', type: 'Cycling', distance: 9238 },
+        {
+          start_date: '2024-03-05T12:55:20Z',
+          type: 'Cycling',
+          distance: 9238,
+        },
         {
           start_date: '2024-03-07T14:10:10Z',
           type: 'Swimming',
           distance: 4523,
         },
         { start_date: '2024-03-09T10:25:00Z', type: 'Run', distance: 4028 },
-        { start_date: '2024-03-11T16:40:30Z', type: 'Cycling', distance: 9427 },
+        {
+          start_date: '2024-03-11T16:40:30Z',
+          type: 'Cycling',
+          distance: 9427,
+        },
         { start_date: '2024-03-13T09:15:55Z', type: 'Run', distance: 5261 },
-        { start_date: '2024-03-15T13:30:25Z', type: 'Cycling', distance: 4265 },
+        {
+          start_date: '2024-03-15T13:30:25Z',
+          type: 'Cycling',
+          distance: 4265,
+        },
         { start_date: '2024-03-17T17:45:10Z', type: 'Run', distance: 9251 },
       ],
       Groups: ['A1000'],
@@ -104,3 +142,16 @@ export class DashBodyComponent {
     },
   ];
 }
+
+/*
+{
+      id: this.mainUser!.id,
+      firstname: this.mainUser!.firstname,
+      lastname: this.mainUser!.lastname,
+      AccessToken: this.mainUser!.AccessToken,
+      RefreshToken: this.mainUser!.RefreshToken,
+      listLength: 200,
+      Activities: this.mainUser!.Activities,
+      Groups: ['A1000', 'A1001'],
+      Friends: [5000, 5001, 5002],
+    }, */
